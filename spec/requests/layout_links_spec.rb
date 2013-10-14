b2d2aa7afa388b651e9f4c6bc84b67bed609c995
Nathaniel J. Soria <njsoria@gmail.com>
2013-10-14 13:40:20 -0400

    more routing, I was finally able to get rid of user.rb!!
    w0000000t!!

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -12,4 +12,19 @@ describe "LayoutLinks" do
   	response.should have_selector('title', :content => "Contact")
   end
 
+  it "should have an About page at '/about'" do
+  	get '/about'
+  	response.should have_selector('title', :content => "About")
+ 	end
+
+ 	it "should have a Help page at '/help'" do
+ 		get '/help'
+ 		response.should have_selector('title', :content => "Help")
+ 	end
+
+ 	it "should have a Login page at '/login'" do
+ 		get '/login'
+ 		response.should have_selector('title', :content => "Login")
+ 	end
+
 end
